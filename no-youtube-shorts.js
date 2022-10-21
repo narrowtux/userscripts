8ffbdca4388405d3f958bf3b555e3b3349233252
Moritz Schmale <narrow.m@gmail.com>
2022-10-21 15:44:45 +0200

    Update no-youtube-shorts.js
    detect shorts player with `_` or `-` in the video id as well
    listen for URL changes because youtube is a SPA

--- no-youtube-shorts.js
+++ no-youtube-shorts.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         no youtube shorts
 // @namespace    http://narrowtux.com/
-// @version      0.1
+// @version      0.2
 // @description  Automatically redirects to the good youtube player with scrubber and without stupid overlays over half the video
 // @author       narrowtux
-// @match        https://www.youtube.com/shorts/*
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
@@ -12,7 +12,16 @@
 (function() {
     'use strict';
 
-    let url = window.location.href;
-    let videoId = url.match(/\/([a-zA-Z0-9]{11})$/)[1]
-    window.location.href = `https://youtube.com/watch?v=${videoId}`
+    function checkUrl() {
+        console.log("Checking for Shorts player...")
+        let url = window.location.href;
+        let videoId = url.match(/\/([a-zA-Z0-9_\-]{11})$/)[1]
+        if (videoId) {
+            console.log("Replacing old player", videoId)
+            window.location.replace(`https://youtube.com/watch?v=${videoId}`)
+        }
+    }
+
+    window.addEventListener("yt-page-data-updated", checkUrl)
+    checkUrl()
 })();
