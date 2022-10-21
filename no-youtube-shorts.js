9d24e2915f257dbc42193bc309332d1fb7c6808a
Moritz Schmale <narrow.m@gmail.com>
2022-10-21 15:49:01 +0200

    Update no-youtube-shorts.js
    actually check if videoId was found on the shorts page
    allow query parameters in the URL

--- no-youtube-shorts.js
+++ no-youtube-shorts.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         no youtube shorts
 // @namespace    http://narrowtux.com/
-// @version      0.2
+// @version      0.3
 // @description  Automatically redirects to the good youtube player with scrubber and without stupid overlays over half the video
 // @author       narrowtux
 // @match        https://www.youtube.com/*
@@ -15,7 +15,7 @@
     function checkUrl() {
         console.log("Checking for Shorts player...")
         let url = window.location.href;
-        let videoId = url.match(/\/([a-zA-Z0-9_\-]{11})$/)[1]
+        let videoId = url.match(/\/shorts\/([a-zA-Z0-9_\-]{11})(\?.*)?$/)[1]
         if (videoId) {
             console.log("Replacing old player", videoId)
             window.location.replace(`https://youtube.com/watch?v=${videoId}`)
