8b23ed19ad9826bca35b814d439982fef111fabd
Moritz Schmale <narrow.m@gmail.com>
2023-02-14 00:28:29 +0100

    Update no-youtube-shorts.js
    automatically removes shorts from subscriptions list

--- no-youtube-shorts.js
+++ no-youtube-shorts.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         no youtube shorts
 // @namespace    http://narrowtux.com/
-// @version      0.3
+// @version      0.4
 // @description  Automatically redirects to the good youtube player with scrubber and without stupid overlays over half the video
 // @author       narrowtux
 // @match        https://www.youtube.com/*
@@ -20,7 +20,20 @@
             console.log("Replacing old player", videoId)
             window.location.replace(`https://youtube.com/watch?v=${videoId}`)
         }
+
+
     }
+
+    let ref;
+    function removeShorts() {
+        console.log("Removing shorts from subscriptions list ...")
+        document.querySelectorAll("span#text.ytd-thumbnail-overlay-time-status-renderer[aria-label=Shorts]").forEach(el => {
+            el.closest(".ytd-grid-renderer").remove()
+            !!ref && clearInterval(ref);
+            ref = null
+        })
+    }
+    ref = setInterval(removeShorts, 200)
 
     window.addEventListener("yt-page-data-updated", checkUrl)
     checkUrl()
